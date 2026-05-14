28b7251528406a7434c3e11c6cc974917d40c919
Murakon2906 <mcray2906writes@gmail.com>
2026-05-14 17:13:06 +0530

    django and react configured

--- frontend/src/DisplayTest.tsx
+++ frontend/src/DisplayTest.tsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function DisplayTest() {
+  const [name, setName] = useState("Guest");
+  const [sex, setSex] = useState("Not Defined");
+  const [age, setAge] = useState(0);
+
+  const fetchdata = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/");
+
+      setName(response.data.name);
+      setSex(response.data.sex);
+      setAge(response.data.age);
+      console.log("Component Loaded");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
   return (
     <div>
-      <h1>Hello from the test component</h1>
+      <h1>
+        Name's {name} . {age} . {sex} . Hard to ignore
+      </h1>
     </div>
   );
 }
