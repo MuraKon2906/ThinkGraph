d694e1ea5cd8bdc831d9859d45637eda056ab97b
Murakon2906 <mcray2906writes@gmail.com>
2026-05-15 22:14:33 +0530

    gemini in backend integrated

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from .views import query_view
+from django.urls import path
+from .views import query_view , api_test 
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/query/", query_view)]
+urlpatterns = [path("admin/", admin.site.urls),
+               path("api/query/", query_view), 
+               path("" , api_test)]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,5 +1,10 @@
+import os 
+from dotenv import load_dotenv 
+from google import genai 
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 @api_view(["POST"])
 def query_view(request):
@@ -15,3 +20,19 @@ def query_view(request):
         print(f"File name : {material.name}")
         print(f"File size : {material.size}")
     return Response( data="Prompt Recieved ")
+
+def api_test(request) : 
+    try : 
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
+        client = genai.Client(api_key=api_key)
+
+        response = client.models.generate_content(model="gemini-2.5-flash" , contents="Explain me quantum computing in easy terms")
+        return JsonResponse({
+            "response" : response.text
+        })
+
+
+       
+    except Exception as e  : 
+        print(e)
