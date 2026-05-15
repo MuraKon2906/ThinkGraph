c2da5c535283a88db292d8eae92c23e09a63ead9
Murakon2906 <mcray2906writes@gmail.com>
2026-05-15 19:32:53 +0530

    prompt div disappears now after uploading

--- frontend/src/ThinkGraphMain.tsx
+++ frontend/src/ThinkGraphMain.tsx
@@ -2,11 +2,12 @@ import axios from "axios";
 import { Form } from "lucide-react";
 import type React from "react";
 import { useState } from "react";
+import ResponseHandlerComponent from "./ResponseHandlerComponent";
 
 function ThinkGraphMain() {
   const [prompt, setPrompt] = useState("");
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
-
+  const [uploadFlag, setUploadFlag] = useState<true | false>(false)
 
 
 
@@ -19,13 +20,7 @@ function ThinkGraphMain() {
     event.currentTarget.style.height = `${event.currentTarget.scrollHeight / 2}px`
   };
 
-
-
   const postPrompt = async () => {
-    const backend_data = {
-      prompt: prompt,
-      date: Date.now(),
-    };
 
     try {
       const formdata = new FormData()
@@ -43,6 +38,7 @@ function ThinkGraphMain() {
       );
 
       console.log(response.data);
+      setUploadFlag(true) // hides the query component 
 
     } catch (err) {
       console.log(err);
@@ -78,30 +74,35 @@ function ThinkGraphMain() {
     setSelectedFile(finalUpload);
   };
 
+
   return (
     <>
       <h1>Think Graph : Your AI Research Search Engine</h1>
       <h2>v:0.0.1</h2>
 
       {/* Prompt and file uploading div */}
-      <div className="flex items-center gap-3">
-        <input type="file" onChange={handleFileUpload} />
 
-        <textarea
-          className="flex-1 resize-none h-10 border rounded-md p-2"
-          placeholder="Knowledge starts here"
-          onChange={handlePromptBox}
-          onKeyDown={handleEnter}
-          value={prompt}
-        />
+      {!uploadFlag &&
+        (<div className="flex items-center gap-3">
+          <input type="file" onChange={handleFileUpload} />
 
-        <button
-          className="px-4 py-2 bg-black text-white rounded-md"
-          onClick={postPrompt}
-        >
-          Enter
-        </button>
-      </div>
+          <textarea
+            className="flex-1 resize-none h-10 border rounded-md p-2"
+            placeholder="Knowledge starts here"
+            onChange={handlePromptBox}
+            onKeyDown={handleEnter}
+            value={prompt}
+          />
+
+          <button
+            className="px-4 py-2 bg-black text-white rounded-md"
+            onClick={postPrompt}
+          >
+            Enter
+          </button>
+        </div>)
+      }
+      {uploadFlag && <ResponseHandlerComponent />}
     </>
   );
 }
